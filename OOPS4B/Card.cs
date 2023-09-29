d727a6ea5a2983200a109cb9f9f0d8fe74aaf35c
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-09-29 21:45:14 +0530

    adding day 4B oops practice problems

---
+++ OOPS4B/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPS4B
+{
+    internal class Card
+    {
+        private String faceName, suit;
+
+        /**
+         * The constructor initializes the instance variables.
+         * @param faceName
+         * @param suit
+         * @param value
+         */
+        public Card(String faceName, String suit)
+        {
+            this.faceName = faceName;
+            this.suit = suit;
+        }
+        /**
+         *@return a String representation of the Card
+         */
+        public override String ToString()
+        {
+            return faceName + " of " + suit;
+        }
+    }
+}
---
+++ OOPS4B/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OOPS4B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DecksOfCards myDeckofCards = new DecksOfCards();
+            Console.WriteLine("-----------------------Deck before shuffling-----------------------------");
+            myDeckofCards.displayDeck();
+
+            myDeckofCards.shuffle();
+            Console.WriteLine("-----------------------Deck After Shuffling------------------------------");
+            myDeckofCards.displayDeck();
+
+            Console.WriteLine("-----------------------53 calls to dealOndeCard()------------------------");
+            for (int i = 0; i < 53; i++)
+            {
+                Card result = myDeckofCards.dealOneCard();
+                if (result == null)
+                {
+                    Console.WriteLine("null");
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
